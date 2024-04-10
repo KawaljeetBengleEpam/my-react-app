96248f4c50fdb381764515acb9abb0fd39931a75
Kawaljeet Bengle <kawaljeet_bengle@epam.com>
2024-04-10 17:31:50 +0530

    Added header logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,17 @@
 import React from 'react';
+import HeaderLogo from '../../assets/headerLogo.jpeg';
 
-const Header = (props) => <h1>My Header {props.name}</h1>;
+function Header(props) {
+	return (
+		<>
+			<div className='row'>
+				<div className='col-sm-6'>
+					<img src={HeaderLogo} height='70' width='50' alt='logo'></img>
+				</div>
+				<div className='col-sm-6'>{props.name} </div>
+			</div>
+		</>
+	);
+}
 
 export default Header;
