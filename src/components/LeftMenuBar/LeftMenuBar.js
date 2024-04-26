194268b34604d8db8f2d7ae64fcf6b853b85357d
Kawaljeet Bengle <kawaljeet_bengle@epam.com>
2024-04-26 18:14:33 +0530

    Implemented Redux with Actions and Reducers and Store

--- src/components/LeftMenuBar/LeftMenuBar.js
+++ src/components/LeftMenuBar/LeftMenuBar.js
@@ -1,19 +1,17 @@
 // LeftMenuBar.js
 import React from 'react';
+import { Provider } from 'react-redux';
+import store from '../../redux/store.js';
+import Counter from '../Counter/Counter';
 
 const LeftMenuBar = () => {
-	const items = [];
 	return (
-		<div>
-			<h1>Select an Item:</h1>
-			<select>
-				{items.map((item, index) => (
-					<option key={index} value={item}>
-						{item}
-					</option>
-				))}
-			</select>
-		</div>
+		<Provider store={store}>
+			<div>
+				<h1>React-Redux Counter App</h1>
+				<Counter />
+			</div>
+		</Provider>
 	);
 };
 
